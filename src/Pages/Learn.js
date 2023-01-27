bfec29365c1343c5205c34d073cd2c168954a7a3
Yogesh Pandey <thedeveloperyug@gmail.com>
2023-01-28 02:38:51 +0530

    Delete src directory

--- src/Pages/Learn.js
+++
@@ -1,9 +0,0 @@
-const Learn = () => {
-  return (
-    <div className='py-24'>
-        <p className='text-3xl bg-orange-500 py-20'>this is learn page</p>
-    </div>
-  )
-}
-
-export default Learn;
